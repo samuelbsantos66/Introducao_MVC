8e8ef4d677023704b1639f312e91f35361e42ce1
Samuel Barbosa Santos <125471851+samuelbsantos66@users.noreply.github.com>
2024-09-05 08:47:51 -0300

    Add files via upload

---
+++ WpfApp1/model/Ideia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.persistência;
+
+namespace WpfApp1.model
+{
+    internal class Ideia
+    {
+        public string area { get; set; } = "";
+        public string ideia { get; set; } = "";
+
+        public float Custo { get; set; } = 0;
+
+        public Boolean cadastrarIdeia(Ideia i)
+        {
+            BD.salvarBD(i);
+            return true;
+        }
+    }
+}
---
+++ WpfApp1/persistência/BD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp1.controller;
+using WpfApp1.model;
+namespace WpfApp1.persistência
+{
+    internal class BD
+    {
+        public static List<BD> mybd = new List<BD>();
+        public static void salvarBD(Ideia i) => mybd.Add(i);
+
+        public static List<Ideia> retomarBD() => mybd;
+    }
+}
